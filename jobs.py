fef89b9f20a3cf9664df5d2ea188f21c5ef628dd
Sean3038 <jkdavid32@gmail.com>
2018-02-16 14:15:05 +0800

    add cron job

--- jobs.py
+++ jobs.py
@@ -16,15 +16,6 @@ class Config(object):
             'start_date': '2013-02-13 00:00',
             'hour': '*/1'
 
-        },
-        {
-            'id': 'job3',
-            'func': 'app_run:d_service',
-            'args': '',
-            'trigger': 'cron',
-            'start_date': '2013-02-13 00:00',
-            'minute': '*/1'
-
         }
     ]
 
